0f586e4bc732336733b136faef4114cf7673e8b0
Miguelerja <miguelerja@gmail.com>
2019-04-09 10:49:56 +0200

    add basic information parsing into html

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 class GithubService {
   async getUserInfoUsers(user) {
     try {
-      const info = await fetch(`https://api.github.com/users/${user}`)
+      const info = await fetch(`https://api.github.com/users/${user}`);
       return info.json();
-    } catch(error){console.warn(error)};
+    } catch(error){return error};
   };
 };
 
 const githubService = new GithubService();
 const searchField = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
+const searchContainer = document.getElementById('search-container');
 
 
-const getUserInfo = () => {
-  console.log(githubService.getUserInfoUsers(searchField.value));
+const getUserInfo = async () => {
+  try{
+    const {
+      avatar_url: avatar,
+      bio,
+      name,
+      email,
+    } = await githubService.getUserInfoUsers(searchField.value);
+    console.log(searchContainer)
+    searchContainer.insertAdjacentHTML('afterend',
+      `<div class="user-info">
+        <img src=${avatar} alt="user picture">
+        <p>@ ${email}</p>
+        <p>${name}</p>
+        <p>${bio}</p>
+      </div>`
+    )
+  }catch(error){
+    searchContainer.innerHTML(
+      `<p class="error-message">Does not exist</p>`
+    );
+  };
 };
-
 
 searchBtn.addEventListener('click', getUserInfo);
